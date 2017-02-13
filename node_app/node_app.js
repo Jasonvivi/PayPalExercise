5e833940d9c86408b2c7dca8a48133ef235be0d2
Jasonvivi <jason.chen.bjtu@gmail.com>
2017-02-12 21:09:42 -0500

    implement echo node app handle request and return json to client

--- node_app/node_app.js
+++ node_app/node_app.js
@@ -1,3 +1,29 @@
 /**
- * Created by jason on 2/12/17.
+ * Created by jason on 2/11/17.
  */
+var http = require('http');
+
+function onRequest(request, response){
+    var body = "";
+    request.on('data', function (chunk) {
+        body += chunk;
+    });
+    request.on('end', function () {
+        console.log('body: ' + body);
+    })
+    response.writeHead(200,{"Content-Type":"application/json"});
+    var data = {};
+    data['response'] = {
+        'listings':[
+            {'title':'Ship to', 'heading':'Kenneth Thomepson', 'subheading':'8114 Grow Drive #9, Cape Neddick, ME 03902'},
+            {'title':'Pay with','heading':'BANK OF AMERICA CHECKING x-5567', 'subheading':'Visa x-4512(backup)'},
+        ],
+        'total':'$28.98',
+        'error':'200',
+    }
+    response.write(JSON.stringify(data));
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server is now running");
